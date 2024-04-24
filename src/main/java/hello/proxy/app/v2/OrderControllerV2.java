4a1d14b40f491a045a469839a05088fa47f2d127
LimJiNa <jn4624@gmail.com>
2024-04-24 22:40:46 +0900

    예제 코드 구현 v2 - noLog 메서드 @GetMapping value 수정

--- src/main/java/hello/proxy/app/v2/OrderControllerV2.java
+++ src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -19,7 +19,7 @@ public class OrderControllerV2 {
         return "ok";
     }
 
-    @GetMapping("/v1/no-log")
+    @GetMapping("/v2/no-log")
     public String noLog() {
         return "ok";
     }
